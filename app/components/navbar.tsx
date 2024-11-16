aea619bb0a8f29c634185876ebd13d90492b7102
giulioGranata <ggiulio89@gmail.com>
2024-11-16 16:32:44 +0100

    pages

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,42 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import ThemeSwitcher from "./theme-switcher";
+
+const pages = [
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Experience",
+    href: "/experience",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
+
+const PageList = () => {
+  const currentPath = usePathname();
+  return (
+    <>
+      {pages.map((page) => (
+        <li key={page.name}>
+          <Link
+            href={page.href}
+            className={currentPath === page.href ? "active" : ""}
+          >
+            {page.name}
+          </Link>
+        </li>
+      ))}
+    </>
+  );
+};
 
 const Navbar = () => {
   return (
@@ -21,15 +57,7 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-              <li>
-                <Link href="/experience">Experience</Link>
-              </li>
-              <li>
-                <Link href="/contact">Contact</Link>
-              </li>
+              <PageList />
             </ul>
           </div>
         </div>
@@ -62,15 +90,7 @@ const Navbar = () => {
         </div>
         <div className="navbar-center">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/experience">Experience</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
+            <PageList />
           </ul>
         </div>
         <div className="navbar-end">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class",
+  darkMode: "selector",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
